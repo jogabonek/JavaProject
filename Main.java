66dadbf8c59da22150ce8334d04ae29626bdf24e
Listia Dewi <jogjabonek4321@gmail.com>
2025-09-07 09:56:14 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,1 @@
-import java.util.Scanner;
-public class Main {
- public static void main(String[] args) {
- // Membuat objek Scanner untuk menerima input dari keyboard
- Scanner scanner = new Scanner(System.in);
- // Meminta pengguna memasukkan nama
- System.out.print("Masukkan nama Anda: ");
- String nama = scanner.nextLine();
- // Menampilkan pesan sambutan yang dipersonalisasi
- System.out.println("Selamat Datang di Java, " + nama + "!");
- // Menutup objek scanner
- scanner.close();
- }
- }
+test
